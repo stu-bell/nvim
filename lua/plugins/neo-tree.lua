caf2fbf2355f1e7a1cf148e198e788ade612501d
Stuart Bell <141676627+stu-bell@users.noreply.github.com>
2025-11-04 16:19:16 +0000

    neo-tree keybining

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,5 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -14,6 +13,15 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    window = {
+      mappings = {
+        -- normal keybindings were mapping l to k, but didn't seem to work in neo-tree, so remap here
+        ['l'] = function(state)
+          vim.cmd 'normal! k'
+        end,
+        ['\\'] = 'close_window',
+      },
+    },
     filesystem = {
       window = {
         mappings = {
