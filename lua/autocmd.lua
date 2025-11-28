514c8c175cef8fbac83e247ca819db89996409fb
Stuart Bell <141676627+stu-bell@users.noreply.github.com>
2025-11-28 09:32:27 +0000

    autocmd for sh LF lineendings

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -3,3 +3,11 @@ vim.api.nvim_create_autocmd('VimEnter', {
   pattern = '*.md',
   command = 'ZenMode',
 })
+
+-- ensure unix line endings for .sh files
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*.sh',
+  callback = function()
+    vim.bo.fileformat = 'unix'
+  end,
+})
