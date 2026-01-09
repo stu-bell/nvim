92476d974088a092fa9777b8c52f0ea12c3ae597
Stuart Bell <141676627+stu-bell@users.noreply.github.com>
2026-01-09 12:45:54 +0000

    terminal keybinding functions

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -22,6 +22,7 @@ return {
     -- <number><C-j> triggers ToggleTerm
     -- 1ToggleTerm togglest terminal 1
     -- 2ToggleTerm toggles terminal 2 etc
+    -- <C-j> won't work in 't' mode as it's interpreted as a newline char in terminals.
     vim.keymap.set('n', '<C-j>', function()
       local count = vim.v.count
       if count == 0 then
@@ -32,18 +33,23 @@ return {
     end, { noremap = true, silent = true })
 
     -- Add keymap for TermNew in normal mode within terminal buffers
+    -- if <C-s>, <C-n> etc clash with terminal programs, remove the 't' mode trigger from keymaps below, to just trigger the keymaps in normal mode 'n'
     vim.api.nvim_create_autocmd('TermOpen', {
       pattern = '*',
       callback = function()
-        vim.keymap.set('n', '<C-n>', function()
+        vim.keymap.set({ 't', 'n' }, '<C-n>', function()
           local name = vim.fn.input 'Terminal name: '
+          vim.cmd 'close' -- close the current terminal window first
           if name ~= '' then
             vim.cmd('TermNew name="' .. name .. '"')
           else
             vim.cmd 'TermNew'
           end
         end, { buffer = 0 })
-        vim.keymap.set('n', '<C-s>', '<cmd>TermSelect<cr>', { buffer = 0 })
+        vim.keymap.set({ 't', 'n' }, '<C-s>', function()
+          vim.cmd 'close' -- close the current terminal window first
+          vim.cmd 'TermSelect'
+        end, { desc = 'Replace terminal with another ToggleTerm' })
       end,
     })
   end,
