4551061073d0578edcb71c16cb79180892482559
Stuart Bell <141676627+stu-bell@users.noreply.github.com>
2026-03-07 16:37:25 +0000

    always unix line endings

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,10 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Always use unix line endings
+vim.opt.fileformat = 'unix'
+vim.opt.fileformats = 'unix'
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
