cb73cac9a5b5877c2364d8b90be76cb016d7b078
Stuart Bell <141676627+stu-bell@users.noreply.github.com>
2026-03-10 13:09:04 +0000

    formatting

---
+++ lua/plugins/format.lua
@@ -0,0 +1,58 @@
+-- conform.nvim - Code formatter plugin
+--
+-- Formats code using external tools (formatters) on save or manually.
+-- Formatters must be installed separately (via Mason or system package manager).
+--
+-- Current formatters:
+--   lua  → stylua  (install: `:MasonInstall stylua`)
+--   json → jq      (install: `winget install jqlang.jq`)
+--
+-- Usage:
+--   <leader>f  → format current buffer manually
+--   :w         → formats automatically on save (except C/C++)
+--   :ConformInfo → show formatter status for current file
+return {
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    opts = {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- install jq on windows with: winget install jqlang.jq
+        json = { 'jq' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
+    },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
